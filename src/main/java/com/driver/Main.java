cb3167ceb36ecd5d592af354c498aa123140f200
Tushar Dey <tushar24dey@gmail.com>
2023-11-16 21:42:56 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly=new RWOnly();
+//        rwOnly.name="Tushar";
+//        System.out.println(rwOnly.name);
+        //error
+        rwOnly.setName("Tushar");
+        String name= rwOnly.getName();
+    }
 }
